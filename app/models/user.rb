6714331534002ecdc7e1bc02b397497f0f7fc417
Mr Jones <jones@rogalabs.com>
2019-11-20 10:57:35 -0300

    added validations to users
    - Added association belongs to address
    
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
+
+  validates :username,
+            presence:  true
+
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: { case_sensitive: false }
+
+  belongs_to :address
 end
