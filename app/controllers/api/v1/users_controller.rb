882facf8a24744d561d78130f8033ef4755fa6dc
Mr Jones <jones@rogalabs.com>
2019-11-22 22:50:38 -0300

    Finalized index method and created method
    - Added new param to render errors better
    
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  deserializable_resource :user, only:  %i[create] 
 
   def index
     command = UserListFinder.call(params)
@@ -7,9 +8,24 @@ class Api::V1::UsersController < ApplicationController
       @users = command.result
       jsonapi_response(@users)
     else
-      render jsonapi_errors: @users.errors.full_messages,
-                             status: :unprocessable_enity
+      render jsonapi_errors: @users.errors.full_messages
     end
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.persisted?
+      jsonapi_response(@user)
+    else
+       render jsonapi_errors: @user.errors
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
--- app/serializable/serializable_user.rb
+++ app/serializable/serializable_user.rb
@@ -1,7 +1,5 @@
 class SerializableUser < JSONAPI::Serializable::Resource
-  type 'users'
-
-  attributes :username, :email
+  attributes :username, :email, :password
 
   belongs_to :address
 end
