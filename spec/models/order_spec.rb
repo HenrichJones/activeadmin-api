08be7b94e67182414e7ca435bf174cdc427c5566
Mr Jones <jones@rogalabs.com>
2019-11-27 09:11:46 -0300

    created order model spec
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:books) }
+    it { should have_many(:books).through(:order_books) }
+  end
+
+  describe 'presence validations' do
+    it { should validate_presence_of(:total) }
+    it { should validate_presence_of(:books) }
+    it { should validate_presence_of(:state) }
+  end
 end
