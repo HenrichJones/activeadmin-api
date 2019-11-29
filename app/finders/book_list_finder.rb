3fab30ae2266af76756aabb5712e6694aca4052d
Mr Jones <jones@rogalabs.com>
2019-11-29 10:24:46 -0300

    Added dynamic filter for books
    - Created new file to filter books,
    - Added action to separate strong params keys,
    - Added new query type to select the query type,
    - Created new action to split key from query type,
    - Return objects flattend
    
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/finders/book_list_finder.rb
+++ app/finders/book_list_finder.rb
@@ -2,14 +2,63 @@ class BookListFinder
   prepend SimpleCommand
 
     def initialize(params)
-      binding.pry 
-      @params = params
+      @key_values = define_parameters(params)
     end
 
     def call
-      Book.where('title ilike :title',
-        title: "%#{@params[:q]}%"
-      )
+      @result = []
+      if @key_values.any?
+        @key_values.each do |k_v|
+          if @result.any?
+            book_ids = @result.flatten.map(&:id)
+            @result  = [
+              Book.where(
+                id: book_ids
+              ).where(
+                "#{define_key(k_v)} ilike ?", query_type(k_v.first, k_v.last)
+              )
+            ]
+          else
+            @result.push([
+              Book.where(
+                "#{define_key(k_v)} ilike ?", query_type(k_v.first, k_v.last)
+              )
+            ])
+          end
+        end
+        @result.flatten
+      else
+        Book.all
+      end
+    end
+
+    def define_parameters params
+      key_value = []
+      keys = params.except(:controller, :action).keys
+      keys.each do |k|
+        key_value.push([k, params[:"#{k}"]] )
+      end
+      key_value
+    end
+
+    def define_key key_value
+      return "avaiable_on" if key_value.first.include?('avaiable')
+      key_value.first.split('_').first
+    end
+
+    def query_type(query, search_value)
+      case
+      when query.include?("equals")
+        "#{search_value}"
+      when query.include?("contains")
+        "%#{search_value}%"
+      when query.include?("starts_with")
+        "#{search_value}%"
+      when query.include?("ends_with")
+        "%#{search_value}"
+      else
+        ""
+      end
     end
 
 end
