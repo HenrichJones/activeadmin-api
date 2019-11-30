763efac7dd18bf3e792d7424fadf613dbaa04c36
Mr Jones <jones@rogalabs.com>
2019-11-30 12:18:26 -0300

    finish delete book method
    - Added new spec to delete user
    
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BooksController < ApplicationController
   deserializable_resource :book, only: %i[create update]
-  before_action :set_book, only: %i[update]
+  before_action :set_book, only: %i[update destroy]
 
   def index
     command = BookListFinder.call(params)
@@ -29,6 +29,11 @@ class Api::V1::BooksController < ApplicationController
     else
       render jsonapi_errors: @book.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @book.destroy
+    return :head, status: :no_content
   end
 
   private
