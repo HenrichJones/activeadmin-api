c404a4a6fe3d22074b65cb8ab8998397061ce051
Mr Jones <jones@rogalabs.com>
2019-12-01 12:09:37 -0300

    removed invalid binding.pry
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,7 +1,6 @@
 module Response
 
   def jsonapi_response(data, status = :created, include = [], fields = {})
-binding.pry
     render  jsonapi: data,
             include: include,
             fields: fields,
